689a3bf9ae1fa619cbc96595a6b66ceb9885b777
syarifsakur <107300773+syarifsakur@users.noreply.github.com>
2024-04-02 02:17:57 +0800

    back

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const sequelize = require("sequelize")
 
-const db = new sequelize(
-    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-    host:process.env.DB_HOST,
+const db = new sequelize("recyeco","root","",{
+    host:"localhost",
     dialect:"mysql"
 })
 
