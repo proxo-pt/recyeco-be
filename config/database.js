197b626d14139acd7360d9d9c59b684ad6ac5d35
adzaky <muhadlim17@gmail.com>
2024-04-16 00:31:02 +0800

    Configure deplyoment files including database config and env variables

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("recyeco-be", "recyeco", "recyeco", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.RECYECO_DB,
+  process.env.RECYECO_DB_USER,
+  process.env.RECYECO_DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.RECYECO_DB_HOST,
+    dialectOptions: {
+      socketPath: process.env.RECYECO_DB_HOST,
+    },
+  }
+);
 
 try {
   db.authenticate();
