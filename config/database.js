9108e155d2d35f13a052ab5f03da0e5ed4ef0528
adzaky <muhadlim17@gmail.com>
2025-11-11 06:52:03 +0800

    fix: apply pg manually

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
+import pg from 'pg';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set');
 }
 
 const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true
